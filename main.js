fdfff066dbb21ec2a1daa3c2a976f7b57b382ff7
AbduqodirAbduvahobov <abduqodirabduvahobov007@gmail.com>
2025-02-14 18:51:53 +0500

    first and last commit

---
+++ main.js
@@ -0,0 +1,66 @@
+// 1 masala
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// 2 masala
+// let n = 15;
+// for (let i = 1; i <= n; i++) {
+//   console.log(i);
+// }
+
+// 3 masala
+
+// let m = 5;
+// let n = 12;
+// for (let i = m; i <= n; i++) {
+//   console.log(i);
+// }
+
+// 4 masala
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// 5 masala
+// let n = 8;
+// for (let i = n; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// 6 masala
+// let m = 15;
+// let n = 8;
+// for (let i = m; i >= n; i--) {
+//   console.log(i);
+// }
+
+// 7 masala
+// let n = 5;
+// let yigindi = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     yigindi = yigindi + i;
+// }
+
+// console.log("Yig‘indi:", yigindi);
+
+// 8 masala
+// let n = 5;
+// let kopaytma = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   kopaytma = kopaytma * i;
+// }
+
+// console.log("Ko‘paytma:", kopaytma);
+
+// 9-masala
+// let n = 5;
+// console.log("Juft sonlar:");
+
+// for (let i = 1; i <= n; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
